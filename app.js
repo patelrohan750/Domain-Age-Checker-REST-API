bb8ea74bad497d1ea3205f34c8b53f586dc32037
unknown <patelrohan750@gmail.com>
2024-02-23 22:22:49 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const domainRoute = require('./routes/router');
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
     res.send("Hi i am Live");
 })
 
+app.use(cors({
+    origin:"*"
+}))
 app.use('/api', domainRoute);
 
 const start = async () => {
